440448e0d0d6255e56fbcba335a1697ba8e027b4
Nicolas Vicko <58487141+nicolasvicko@users.noreply.github.com>
2024-03-28 02:03:43 -0300

    Firts Commit

---
+++ API-REST-with-spring-boot/src/main/java/dev/vicko/StartUp.java
@@ -0,0 +1,13 @@
+package dev.vicko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartUp.class, args);
+	}
+
+}
